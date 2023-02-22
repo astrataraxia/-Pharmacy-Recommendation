eebd7ff6b74a1596e41704880638c0278c8894d5
stone <astrataraxia@gmail.com>
2023-02-22 15:06:45 +0900

    first commit

---
+++ src/main/java/com/example/roadproject/RoadProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.roadproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoadProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/roadproject/RoadProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.roadproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
